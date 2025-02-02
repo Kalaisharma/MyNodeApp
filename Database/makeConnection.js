8286aafc9a8a8fdbeab437b47bef1d72d097d10f
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-02-02 15:27:04 +0530

    update database connection port to 11968

--- Database/makeConnection.js
+++ Database/makeConnection.js
@@ -2,7 +2,7 @@ const config = {
   host: "junction.proxy.rlwy.net",
   user: "root", // Use a string for the username
   password: "glqrDDibgVXklvIWwAdZeqOjxdSXwZqv",
-  port: 3306,
+  port: 11968,
   database: "railway", // Change this to your actual database name
   connectTimeout: 10000
 };
