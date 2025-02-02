3058ecb142c0f71c6dc512fecf6056cadb68c582
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-02-02 14:41:46 +0530

    update database host to include port for connection

--- Database/makeConnection.js
+++ Database/makeConnection.js
@@ -1,5 +1,5 @@
 const config = {
-  host: "junction.proxy.rlwy.net",
+  host: "junction.proxy.rlwy.net:11968",
   user: "root", // Use a string for the username
   password: "glqrDDibgVXklvIWwAdZeqOjxdSXwZqv",
   port: 3306,
