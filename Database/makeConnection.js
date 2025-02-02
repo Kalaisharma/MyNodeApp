205d9841dae09c153f6afa675ec45578560c13ae
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-02-02 14:00:50 +0530

    update database connection settings for Railway environment

--- Database/makeConnection.js
+++ Database/makeConnection.js
@@ -1,9 +1,9 @@
 const config = {
-  host: "sql111.infinityfree.com",
-  user: "if0_38223323", // Use a string for the username
-  password: "TsVXlkJV8bM",
+  host: "mysql.railway.internal",
+  user: "root", // Use a string for the username
+  password: "glqrDDibgVXklvIWwAdZeqOjxdSXwZqv",
   port: 3306,
-  database: "if0_38223323_userdata", // Change this to your actual database name
+  database: "railway", // Change this to your actual database name
 };
 module.exports={config}
 
